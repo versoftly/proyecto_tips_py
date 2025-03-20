4e5bde9f7bd62a154195b7e2347893a0a121b7e6
Ramiro G <mostlyphpsoftware@gmail.com>
2025-03-20 10:43:57 -0600

    gettig the subtotal

--- python.py
+++ python.py
@@ -1,1 +1,18 @@
 print ("Tips Program")
+'''
+    bill = 150
+    porcentaje = 12
+    operacion = 12 / 100
+    tip = operacion * bill
+    total = bill + tip
+'''
+bill = input ("total del bill ? ")
+bill = float(bill)
+porcentaje = input ("tip % : 10 , 12 o 15 ")
+porcentaje = int(porcentaje)
+operacion_1 = porcentaje / 100
+tip = operacion_1 * bill
+subTotal = bill + tip
+subTotal = str(subTotal)
+print ("bill + tip = " + subTotal)
+# personas = input ("Entre cuantos ?")
