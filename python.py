c2edecdab0900723a6c3bc58cbc89ee3115f2d7d
Ramiro G <mostlyphpsoftware@gmail.com>
2025-03-20 10:51:36 -0600

    done

--- python.py
+++ python.py
@@ -12,7 +12,13 @@ porcentaje = input ("tip % : 10 , 12 o 15 ")
 porcentaje = int(porcentaje)
 operacion_1 = porcentaje / 100
 tip = operacion_1 * bill
-subTotal = bill + tip
-subTotal = str(subTotal)
-print ("bill + tip = " + subTotal)
-# personas = input ("Entre cuantos ?")
+Total = bill + tip
+Total = str(Total)
+print ("bill + tip = " + Total)
+Total = float (Total)
+personas = input ("Entre cuantos ? ")
+personas = int(personas)
+TxPersona = Total / personas
+TxPersona = round(TxPersona,2)
+TxPersona = str(TxPersona)
+print("A cada persona le toca : " + TxPersona)
